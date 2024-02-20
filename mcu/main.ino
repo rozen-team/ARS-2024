a4c2840d635b22f1bb6073f29ce5362e34e7b8bb
= <=>
2024-02-20 12:56:01 +1000

    add ms5837 to main.ino

--- mcu/main.ino
+++ mcu/main.ino
@@ -1,29 +1,50 @@
+#include <Wire.h>
 #include <OLED_I2C.h>
+#include <MS5837.h>
 
+
+MS5837 sens;
 OLED oled(SDA, SCL, 8);
 
 extern uint8_t letters[];
 extern uint8_t numbers[];
 
 
-void display_values(float depth, float tempr) {
+float depth, tempr;
+void read_sens() {
+	sens.read();
+	
+	depth = sens.depth();
+	tempr = sens.temperature();
+}
+
+
+void display_values(float _depth, float _tempr) {
 	oled.setFont(letters);
 	oled.print("D:", LEFT, 0);
 	oled.print("T:", LEFT, 40);
 
 	oled.setFont(numbers);
-	oled.printNumF(depth, 2, CENTER, 0);
-	oled.printNumF(tempr, 2, CENTER, 40);
+	oled.printNumF(_depth, 2, CENTER, 0);
+	oled.printNumF(_tempr, 2, CENTER, 40);
 
 	oled.update();
 }
 
 
-
-
 void setup() {
 	oled.begin();
+	Wire.begin();
+	Serial.begin();
+
+	while(!sens.init()) {}
+	sens.setModel(MS5837::MS5837_30BA);
+	sens.setFluidDensity(997);	
 }
+
+
+
+
 
 
 
