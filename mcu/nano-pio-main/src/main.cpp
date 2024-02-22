9124a1bf4e2344449b093327cea387f782f1e527
= <=>
2024-02-22 11:38:09 +1000

    main code, small danger

--- mcu/nano-pio-main/src/main.cpp
+++ mcu/nano-pio-main/src/main.cpp
@@ -6,9 +6,10 @@
 
 #define pixels_cnt 6
 #define pixels_pin 2
-#define pixels_brightness 2
-#define oled_brightness 20
+#define pixels_brightness 10
+#define oled_brightness 10
 #define depth_step 0.25
+#define ms_offset 0.48
 
 
 MS5837 sens;
@@ -19,10 +20,11 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(pixels_cnt, pixels_pin, NEO_GRB + N
 extern uint8_t BigNumbers[];
 extern uint8_t SmallFont[];
 
-const uint32_t pixels_color[3] = {
+const uint32_t pixels_color[4] = {
     pixels.Color(0, 100, 0),   // 0 -> Green
     pixels.Color(100, 100, 0), // 1 -> Yellow
     pixels.Color(100, 0, 0),   // 2 -> Red
+    pixels.Color(0, 0, 0)
 };
 double depth, tempr;
 bool in_danger;
@@ -32,12 +34,11 @@ uint8_t depth_stage;
 void read_sens() {
 	sens.read();
 	
-	depth = sens.depth();
+	depth = sens.depth() - ms_offset;
   	tempr = sens.temperature();
 
-    depth_stage = (uint8_t)depth / depth_step;
-    if (depth_stage > 6 || depth > 1.5) 
-        in_danger = true;
+    depth_stage = int(max((float)depth, 0.0f) / depth_step);
+    depth_stage > 6 || depth > 1.5 ? in_danger = true : in_danger = false;
 }
 void for_dataset() {
     depth = (float)random(1000, 9999)/100;
@@ -45,6 +46,7 @@ void for_dataset() {
 }
 
 void display_values(double _depth, double _tempr, bool serial = 0) {
+    oled.setBrightness(oled_brightness);
   	oled.setFont(SmallFont);
 	oled.print("D:", LEFT, 0);
     oled.print("T:", LEFT, 40);
@@ -61,8 +63,9 @@ void display_values(double _depth, double _tempr, bool serial = 0) {
     }
 }
 void display_danger() {
+    oled.setBrightness(oled_brightness);
     oled.setFont(SmallFont);
-    oled.print("DANGER", CENTER, CENTER);
+    oled.print("DANGER", 50, 20);
     oled.update();
 }
 void display_danger_u8g() {
@@ -81,14 +84,20 @@ void led_indicate(uint8_t ind) {
         pixels.setPixelColor(pixels_cnt-i-1, pixels_color[(int)(ind-1)/2]);
 
     pixels.show();
+
 }
 void led_in_danger() {
-    for(int i=0; i<pixels_cnt; ++i) 
-        pixels.setPixelColor(i+1, pixels_color[2]);
-    pixels.show(); delay(200);
+    led_indicate(6);
+    delay(200);
 
-    pixels.clear(); 
-    pixels.show(); delay(200);
+    for(int i=0; i<6; ++i) 
+        pixels.setPixelColor(pixels_cnt-i-1, pixels_color[3]);
+
+    pixels.show();  
+    delay(200);
+}
+
+void ddanger() {
 
 }
 
@@ -108,18 +117,18 @@ void setup() {
     sens.setModel(MS5837::MS5837_30BA);
     sens.setFluidDensity(997);	
 
-    if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
-        u8g.setColorIndex(255);     // white
-    }
-    else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
-        u8g.setColorIndex(3);         // max intensity
-    }
-    else if ( u8g.getMode() == U8G_MODE_BW ) {
-        u8g.setColorIndex(1);         // pixel on
-    }
-    else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
-        u8g.setHiColorByRGB(255,255,255);
-    }
+    // if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    //     u8g.setColorIndex(255);     // white
+    // }
+    // else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    //     u8g.setColorIndex(3);         // max intensity
+    // }
+    // else if ( u8g.getMode() == U8G_MODE_BW ) {
+    //     u8g.setColorIndex(1);         // pixel on
+    // }
+    // else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    //     u8g.setHiColorByRGB(255,255,255);
+    // }
 
     oled.setBrightness(oled_brightness);
     pixels.setBrightness(pixels_brightness);
@@ -128,16 +137,16 @@ void loop() {
     read_sens();   
 
     if(in_danger) {
-        clear();
-        display_danger();
+        clear();                
 
-        auto tim = millis();
-        while(tim+4000 > millis()) 
-            led_in_danger();
+        display_danger();
+        auto tmr = millis();
+        while(tmr+4000 > millis()) { led_in_danger(); }
+
 
     } else {
-        display_values(depth, tempr, 1);
         led_indicate(depth_stage);
+        display_values(depth, tempr);
     }
 
     delay(50);
